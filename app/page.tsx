876e1477a21d597d4b7d0da989780af0cb2907f7
Fernando Pereira <comprasdfernando@gmail.com>
2025-11-03 21:24:05 -0300

    Adiciona página inicial temporária

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        textAlign: "center",
+        marginTop: "100px",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1>🍗 Gigante dos Assados</h1>
+      <p>Site no ar com sucesso!</p>
+      <p>Em breve cardápio e pedidos online 😋</p>
+    </main>
+  );
+}
